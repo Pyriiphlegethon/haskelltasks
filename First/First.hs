f3182a978ab57309843b715fd9907953dd588dc2
Pyriiphlegethon <8017571+Pyriiphlegethon@users.noreply.github.com>
2022-06-05 14:40:21 +0300

    first tasks added

--- First/First.hs
+++ First/First.hs
@@ -4,4 +4,20 @@ sign x
     | x < 0 = (-1)
     | x > 0 = (1)
     | otherwise = 0
-    
+
+infixl 7 |-|
+x |-| y = abs(x-y)
+
+
+discount :: Double -> Double -> Double -> Double
+discount limit proc sum = if sum >= limit then sum * (100 - proc) / 100 else sum
+
+standardDiscount :: Double -> Double
+standardDiscount = discount 1000 5
+
+import Data.Char
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int x y = if isDigit x && isDigit y then digitToInt x * 10 + digitToInt y else 100
+
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist p1 p2 = sqrt $ (fst p2 - fst p1) ^ 2 + (snd p2 - snd p1) ^ 2 
